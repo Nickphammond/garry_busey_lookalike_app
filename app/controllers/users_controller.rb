573d0174e71f521bbbb94297bd85101f5341e4cf
Nicholas Hammond <hammondn649@gmail.com>
2021-03-11 13:07:32 +1100

    allow editing of profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:user_profile]
+    before_action :set_user, only: [:user_profile, :edit, :update]
     before_action :authenticate_user!
 
     def user_profile
@@ -8,10 +8,24 @@ class UsersController < ApplicationController
     def edit
     end
 
+    def update
+        if @user.update(user_params)
+            redirect_to "/profile"
+        else
+         render "edit", error: "Please try again"
+        end
+    end
+
+    
+
     private
 
     def set_user
         @user = current_user
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :address)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   resources :look_a_likes
   resources :events
+  resources :users
   devise_for :users,  path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/", to: "pages#home", as: :root
 
   get "/profile", to: "users#user_profile", as: "user_profile"
+
+  post "/profile", to: "users#update"
+  patch "/profile", to: "users#update"
+  put  "/profile", to: "users#update"
   get "/profile/edit", to: "users#edit", as: "edit_profile"
 
   get "/lookalikes", to: "look_a_likes#index"
