a3a6729fe7c14b7d24512776a6eefa73226d5032
Nicholas Hammond <hammondn649@gmail.com>
2021-03-16 00:37:59 +1100

    add destroy to events_look_a_likes in event controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
     belongs_to :address
     belongs_to :user
 
-    has_many :events_look_a_likes
+    has_many :events_look_a_likes, dependent: :destroy 
     has_many :look_a_likes, through: :events_look_a_likes
 
     accepts_nested_attributes_for :address
